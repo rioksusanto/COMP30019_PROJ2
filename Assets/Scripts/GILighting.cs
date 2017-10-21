df86f793541192cd934757a688fbadd4baaa4b83
rchkmchk <mcheng3@student.unimelb.edu.au>
2017-10-21 14:13:23 +1100

    Arena Shader and standard shader

--- Assets/Scripts/GILighting.cs
+++ Assets/Scripts/GILighting.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GILighting : MonoBehaviour {
-    public Renderer renderer;
-	// Use this for initialization
-	void Start () {
+    public new Renderer renderer;
+    // Use this for initialization
+    void Start () {
         renderer = GetComponent<Renderer>();
 
     }
@@ -13,7 +13,8 @@ public class GILighting : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float intensity = 5.0f;
-        Color final = Color.cyan * Mathf.LinearToGammaSpace(intensity);
+        Color baseColor = new Color(0, 244, 255, 255);
+        Color final = baseColor * Mathf.LinearToGammaSpace(intensity);
         renderer.material.SetColor("_EmissionColor", final);
         renderer.UpdateGIMaterials();
         
